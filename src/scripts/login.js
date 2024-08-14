029f29c85c89a33781288d18a86d7c534ca1f273
manystarsapart <manystarsapart@gmail.com>
2024-08-14 22:02:34 +0800

    dev: make login page working as is but badly

---
+++ src/scripts/login.js
@@ -0,0 +1,19 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "test" && password === "testpassword") {
+        loginErrorMsg.classList.add("opacity-0");
+        loginErrorMsg.classList.remove("opacity-100");
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.classList.remove("opacity-0");
+        loginErrorMsg.classList.add("opacity-100");
+    }
+})
